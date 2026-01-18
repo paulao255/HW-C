b83fa54acea17cc1af4f8b6cac79990c6c6e1b46
paulao255 <contactpaulao@gmail.com>
2026-01-18 09:29:00 -0300

    Add LICENSE, README.md, CMakeLists.txt, .gitignore and src/Main.c in main branch (2026/01/18).

---
+++ src/Main.c
@@ -0,0 +1,18 @@
+/* Include guard: */
+#ifndef HW_C
+#define HW_C
+
+/* Importations: */
+#include <stdio.h>
+
+
+/* Main code: */
+int main(void)
+{
+	puts("Hello, World!");
+
+	return 0;
+}
+
+/* End code: */
+#endif
